116d37149ebe284a02a2cefee5cd9179f6afdf93
Jinsoo Bae <jinsoobae@Jinsooui-MacBookPro.local>
2022-07-21 23:51:20 +0900

    feat/대시보드 사이드바 추가

---
+++ src/component/common/Sidebar/Sidebar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const Side = styled.div`
+  border-right: 1px solid #e0e0e0;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 20%;
+`;
+
+// 메뉴명을 보여줄 div
+export const SbTitle = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 5px;
+  height: 32px;
+  &:hover {
+    background-color: #f6f6f2;
+    cursor: pointer;
+    border-right: solid 3px;
+  }
+`;
+
+const Menu = styled.div`
+  min-width: 100%;
+  width: auto;
+  height: auto;
+  min-height: 80vh;
+  font-size: 18px;
+`;
+
+const Sidebar = () => {
+  const menus = [
+    { name: "샘플1", path: "/" },
+    { name: "샘플2", path: "/mylist" },
+  ];
+  return (
+    <Side>
+      <Menu>
+        {menus.map((menu, index) => {
+          return (
+            <NavLink
+              exact
+              style={{ color: "gray", textDecoration: "none" }}
+              to={menu.path}
+              key={index}
+              activeStyle={{ color: "black" }}
+            >
+              <SbTitle>menu</SbTitle>
+            </NavLink>
+          );
+        })}
+      </Menu>
+    </Side>
+  );
+};
+
+export default Sidebar;
