0bbbcf5ca916021189a62d15252621c22306233e
dev-sese <dev.shhong@gmail.com>
2022-09-26 20:38:38 +0900

    ADD - drag and drap feature

--- src/component/textarea/TextArea.js
+++ src/component/textarea/TextArea.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PageTitle from "./PageTitle";
 import TextBox from "./TextBox";
 
+import update from "immutability-helper";
 import { block_list } from "@src/testdata";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function TextArea() {
+  const [textBoxs, setTextBoxs] = useState(block_list);
+
+  const moveTextBox = useCallback((dragIndex, hoverIndex) => {
+    setTextBoxs((prevBox) =>
+      update(prevBox, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, prevBox[dragIndex]],
+        ],
+      })
+    );
+  }, []);
+
+  const renderTextBox = useCallback((data, index) => {
+    return (
+      <TextBox
+        key={data.block_id}
+        index={index}
+        id={data.block_id}
+        text={data.content}
+        moveTextBox={moveTextBox}
+      />
+    );
+  }, []);
+
   return (
     <div className="w-4/6 bg-gray-50 border-2 border-red p-6 divide-y-2">
       <PageTitle></PageTitle>
-      <div className="py-3 space-y-1">
-        {block_list.map((data, index) => (
-          <TextBox data={data.content} key={data.block_id} />
-        ))}
-      </div>
+      <DndProvider backend={HTML5Backend}>
+        <div className="py-3 space-y-1">
+          {textBoxs.map((data, index) => renderTextBox(data, index))}
+        </div>
+      </DndProvider>
     </div>
   );
 }
--- src/component/textarea/TextBox.js
+++ src/component/textarea/TextBox.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useRef } from "react";
+import { ItemTypes } from "./ItemTypes";
+import { useDrag, useDrop } from "react-dnd";
+import styled from "daisyui/dist/styled";
 
-function TextBox(props) {
+function TextBox({ text, index, id, moveTextBox }) {
+  const ref = useRef(null);
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: ItemTypes.TEXTBOX,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return null;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return null;
+      }
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return null;
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return null;
+      }
+      moveTextBox(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.TEXTBOX,
+    item: { id, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
+
   return (
-    <div className="rounded-lg hover:bg-gray-100 p-1 px-2">
-      <span>{props.data}</span>
+    <div
+      className="rounded-lg hover:bg-gray-100 p-1 px-2"
+      ref={ref}
+      style={{ ...styled, opacity }}
+      data-handler-id={handlerId}
+    >
+      <span>{text}</span>
     </div>
   );
 }
